638eeea6051e36d9e4860aef51d2ffaa578c33f9
Raúl España <uo278036@uniovi.es>
2023-03-01 15:32:46 +0100

    Esqueleto y primeros pasos

---
+++ src/main/java/com/uniovi/sdimywallapop/SdiMywallapopApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdimywallapop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiMywallapopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdiMywallapopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdimywallapop/SdiMywallapopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdimywallapop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiMywallapopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
